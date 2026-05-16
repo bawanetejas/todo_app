8f85b30b613bd76030d79dcdd6f55feb6b34c93d
bawanetejas <tejasbawane221@gmail.com>
2026-05-16 14:44:06 +0530

    feat: add edit todo functionality

--- script.js
+++ script.js
@@ -28,14 +28,24 @@ function renderTodo() {
         btn.innerText = "delete"
         btn.addEventListener("click", () => {
             allTodos.splice(i, 1);
-
             renderTodo();
         })
+        const editBtn = document.createElement("button")
+        editBtn.innerText = "Edit";
+        editBtn.addEventListener("click", () => editTodo(i))
         const row = document.createElement("ul")
         row.className = "row"
         row.appendChild(p);
+        row.appendChild(editBtn)
         row.appendChild(btn);
         todos.appendChild(row);
     })
 
+}
+
+function editTodo(i) {
+    const val = prompt("Edit todo")
+    if (!val || val == "") return
+    allTodos[i] = val
+    renderTodo();
 }
